eeaec37cc87d920610184f0e1ef8bec524380335
hosenur <hosenur.dev@gmail.com>
2025-12-07 04:25:04 +0530

    refactor: add Prisma accelerate extension to client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const connectionString = process.env.DATABASE_URL;
 
 const adapter = new PrismaPg({ connectionString });
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({ adapter }).$extends(withAccelerate());
